71ce340cf1ce579da00f5e379a167ad2d795a095
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2021-12-13 20:38:16 +0100

    Create searchdata.js

---
+++ src/classes/searchdata.js
@@ -0,0 +1,37 @@
+class Video{
+    constructor(data){
+        this.id = data.videoId;
+        this.url = `https://www.youtube.com/watch?v=${this.id}`;
+        this.title = data.title.runs[0].text;
+        this.author = data.ownerText.runs[0].text;
+        this.channelId = data.ownerText.runs[0].navigationEndpoint.browseEndpoint.browseId;
+        this.channelURL = `https://www.youtube.com${data.ownerText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url}`;
+
+        if(data.lengthText){
+            this.lengthText = data.lengthText.simpleText;
+            const length = this.lengthText.split(':').reverse();
+            var timestampLength = 0;
+            for(var i = 0; i < length.length; i++){
+            const l = length[i];
+            if(i < 3) timestampLength += l * Math.pow(60, i) * 1000;
+            else if(i === 3) timestampLength += l * 24 * Math.pow(60, i) * 1000;
+            }
+            this.length = timestampLength;
+        } else {
+            this.lengthText = 'Unknown length';
+            this.length = 0;
+        }
+
+        if(data.viewCountText){
+            this.viewsText = data.viewCountText.simpleText;
+            this.views = Number(data.viewCountText.simpleText.toLowerCase().split(',').join('').split(' ')[0]);
+        } else {
+            this.viewText = 'Unknown views';
+            this.views = 0;
+        }
+        
+        this.thumbnail = data.thumbnail.thumbnails[0].url;
+    }
+}
+
+module.exports = Video;
