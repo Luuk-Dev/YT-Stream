adcaf2e788947207502c7b1d1b4e386b99c93152
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2022-03-11 18:19:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-module.exports = {
-    validateURL: require('./validate.js').validateURL,
-    validateID: require('./validate.js').validateID,
-    getInfo: require('./info.js').getInfo,
-    getID: require('./convert.js').getID,
-    getURL: require('./convert.js').getURL,
-    stream: require('./stream/createstream.js').stream,
-    search: require('./search/index.js').search
+const validate = require('./validate.js');
+const getInfo = require('./info.js').getInfo;
+const convert = require('./convert.js');
+const stream = require('./stream/createstream.js').stream;
+const search = require('./search/index.js').search;
+
+class YTStream{
+  constructor(){
+    this.validateURL = function(...args){
+      return validate.validateURL(this, ...args)
+    };
+    this.validateID = function(...args){
+      return validate.validateID(this, ...args);
+    };
+    this.getInfo = function(...args){
+      return getInfo(this, ...args);
+    };
+    this.getID = function(...args){
+      return convert.getID(this, ...args);
+    };
+    this.getURL = function(...args){
+      return convert.getURL(this, ...args);
+    };
+    this.stream = function(...args){
+      return stream(this, ...args);
+    }
+    this.search = function(...args){
+      return search(this, ...args);
+    }
+    this.cookie = null;
+		this.userAgent = null;
+  }
 }
+
+var ytstream = new YTStream();
+
+module.exports = ytstream;
