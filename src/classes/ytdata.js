c6431ed155fc01b86f9ca5e91b1f79f6ccfb4d85
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2022-05-06 11:49:18 +0200

    Update ytdata.js

--- src/classes/ytdata.js
+++ src/classes/ytdata.js
@@ -1,34 +1,52 @@
 class YouTubeData{
-    constructor(data, html5player, userA){
+    constructor(data, html5player, headers){
         var videoDetails = data.videoDetails;
         var microformat = data.microformat.playerMicroformatRenderer;
-        this.id = videoDetails.videoId,
-        this.url = `https://www.youtube.com/watch?v=${videoDetails.videoId}`,
-        this.author = videoDetails.author,
-        this.title = videoDetails.title,
-        this.description = {
-            short: videoDetails.shortDescription,
-            full: microformat.description.simpleText
-        }
-        this.familySafe = microformat.isFamilySafe,
-        this.availableCountries = microformat.availableCountries,
-        this.category = microformat.category,
-        this.thumbnails = videoDetails.thumbnail.thumbnails,
-        this.default_thumbnail = videoDetails.thumbnail.thumbnails[videoDetails.thumbnail.thumbnails.length - 1],
-        this.uploaded = microformat.publishDate,
-        this.duration = Number(videoDetails.lengthSeconds),
-        this.views = Number(videoDetails.viewCount),
+        this.id = videoDetails.videoId;
+        this.url = `https://www.youtube.com/watch?v=${videoDetails.videoId}`;
+        this.author = videoDetails.author;
+        this.title = videoDetails.title;
+        this.description = videoDetails.shortDescription;
+        this.embedUrl = microformat.embed.iframeUrl;
+        this.familySafe = microformat.isFamilySafe;
+        this.availableCountries = microformat.availableCountries;
+        this.category = microformat.category;
+        this.thumbnails = videoDetails.thumbnail.thumbnails;
+        this.default_thumbnail = videoDetails.thumbnail.thumbnails[videoDetails.thumbnail.thumbnails.length - 1];
+        this.uploaded = microformat.publishDate;
+        this.duration = Number(videoDetails.lengthSeconds);
+        this.views = Number(videoDetails.viewCount);
+
+        var viewsText = Number(videoDetails.lengthSeconds).toString();
+        viewsText = viewsText.split('').reverse();
+        viewsText = viewsText.reduce((arr, number, index) => {
+            if((index + 1) % 3 === 0){
+                arr.push(number);
+                arr.push('.');
+            } else {
+                arr.push(number);
+            }
+            return arr;
+        }, []);
+        viewsText = viewsText.reverse().join('');
+
+        this.viewsText = viewsText;
         this.channel = {
           author: videoDetails.author,
           id: videoDetails.channelId,
           url: `https://www.youtube.com/channel/${videoDetails.channelId}`
-        }
-        this.formats = [],
+        };
+        this.formats = [];
         this.html5player = html5player;
 
         this.formats.push(...(data.streamingData.formats || []));
         this.formats.push(...(data.streamingData.adaptiveFormats || []));
-	this.userAgent = userA;
+		this.userAgent = headers['user-agent'];
+        if(typeof headers['cookie'] === 'string'){
+            this.cookie = headers['cookie'];
+        } else {
+            this.cookie = null;
+        }
     }
 }
 
