17ca4aa59202b28d47dee0eae78216005a202994
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2022-10-18 17:51:57 +0200

    Update convert.js

--- src/convert.js
+++ src/convert.js
@@ -1,27 +1,27 @@
-const validate = require('./validate.js').validateURL;
+const { validateURL, validateID, validatePlaylistID } = require('./validate.js');
 const _getURL = require('./request/url.js');
 
 function getID(ytstream, url){
   if(typeof url !== 'string') throw new Error(`URL is not a string`);
-  if(!validate(ytstream, url)) return undefined;
+  if(!validateURL(ytstream, url)) return undefined;
 
   const parsed = _getURL(url);
-  
+
   const host = parsed.hostname.toLowerCase().split('www.').join('');
 
   let ytid;
   if(host === `youtu.be`) ytid = parsed.pathname.split('/').join('');
   else if(host === `youtube.com` || host === 'music.youtube.com'){
-    if(parsed.pathname.startsWith('/watch')) ytid = parsed.searchParams.get('v');
+    if(parsed.pathname.startsWith('/watch')) ytid = parsed.searchParams.get('v') || null;
     else if(parsed.pathname.startsWith('/embed/')) ytid = parsed.pathname.split('/embed/').join('');
     else if(parsed.pathname.startsWith('/v/')) ytid = parsed.pathname.split('/v/').join('');
     else if(parsed.pathname.startsWith('/shorts/')) ytid = parsed.pathname.split('/shorts/').join('');
+    else if(parsed.pathname.startsWith('/playlist')) ytid = parsed.searchParams.get('list') || null;
     else ytid = null
   } else ytid = null;
 
-  let reg = /^[A-Za-z0-9_-]*$/;
-  if(!reg.test(ytid)) return undefined;
-  return ytid;
+  if(validateID(ytstream, ytid) || validatePlaylistID(ytstream, ytid)) return ytid;
+  else return undefined;
 }
 
 function getURL(ytstream, id){
