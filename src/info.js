61cc09d860c3b287cd2efddd7c6eace9a1f45f17
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-06-25 20:43:35 +0200

    Update info.js

--- src/info.js
+++ src/info.js
@@ -36,7 +36,7 @@ function getInfo(ytstream, url){
 
     const yturl = `https://www.youtube.com/watch?v=${ytid}&has_verified=1`;
 
-    const userA = typeof ytstream.userAgent === 'string' ? ytstream.userAgent : userAgent();
+		const userA = typeof ytstream.userAgent === 'string' ? ytstream.userAgent : userAgent();
 
     let headers = { 
       'accept-language': 'en-US,en-IN;q=0.9,en;q=0.8,hi;q=0.7',
@@ -73,6 +73,10 @@ function getInfo(ytstream, url){
         return;
       }
       var seperate = res.split(`,"uploadDate":`);
+      if(seperate.length < 2){
+        reject(`The YouTube song has no initial player response`);
+        return;
+      }
       var uploadDateValue = seperate[1].split("}}")[0];
       res = seperate[0] + `,"uploadDate":${uploadDateValue}}}}`;
       var data = JSON.parse(res);
