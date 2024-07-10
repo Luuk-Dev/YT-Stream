5c8da3bb543dab7dec5445f98d5f90712e11e366
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-07-10 18:19:47 +0200

    Update playlistvideo.js

--- src/classes/playlistvideo.js
+++ src/classes/playlistvideo.js
@@ -4,10 +4,15 @@ class PlaylistVideo{
         this.video_id = videodata.videoId;
         this.video_url = `https://www.youtube.com/watch?v=${videodata.videoId}`;
         this.position = parseInt(videodata.index.simpleText);
-        this.length_text = videodata.lengthText;
+        this.length_text = videodata.lengthText.simpleText;
         this.length = parseInt(videodata.lengthSeconds) * 1000;
         this.thumbnails = videodata.thumbnail.thumbnails;
         this.default_thumbnail = videodata.thumbnail.thumbnails[videodata.thumbnail.thumbnails.length - 1];
+        this.channel = {
+            author: videodata.shortBylineText.runs[0].text,
+            id: videodata.shortBylineText.runs[0].navigationEndpoint.browseEndpoint.browseId,
+            url: `https://www.youtube.com/channel/${videodata.shortBylineText.runs[0].navigationEndpoint.browseEndpoint.browseId}`
+          };
         this.playlist_id = videodata.navigationEndpoint.watchEndpoint.playlistId;
         this.playlist_url = data.microformat.microformatDataRenderer.urlCanonical;
     }
