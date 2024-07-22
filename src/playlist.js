34598b96acc7ca4f27546c123add9d3325b0631d
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-07-22 21:20:58 +0200

    Update playlist.js

--- src/playlist.js
+++ src/playlist.js
@@ -1,9 +1,10 @@
 const { validatePlaylistURL } = require("./validate.js");
 const _url = require('./request/url.js');
-const { requestCallback } = require("./request/index.js");
+const { requestCallback, request } = require("./request/index.js");
 const userAgent = require("./request/useragent.js").getRandomUserAgent;
 const { getID } = require("./convert.js");
 const Playlist = require("./classes/playlist.js");
+const genClientInfo = require('./genClient.js');
 
 function getPlaylist(ytstream, url){
     return new Promise(async (resolve, reject) => {
@@ -21,46 +22,67 @@ function getPlaylist(ytstream, url){
 
         if(listId === null) return reject(`Invalid YouTube url`);
 
-        const userA = typeof ytstream.userAgent === 'string' ? ytstream.userAgent : userAgent();
+        if(ytstream.preference === 'scrape'){
+            const userA = typeof ytstream.userAgent === 'string' ? ytstream.userAgent : userAgent();
 
-        let headers = { 
-            'accept-language': 'en-US,en-IN;q=0.9,en;q=0.8,hi;q=0.7',
-            'user-agent' : userA,
-        };
+            let headers = { 
+                'accept-language': 'en-US,en-IN;q=0.9,en;q=0.8,hi;q=0.7',
+                'user-agent' : userA,
+            };
 
-        for(const header in ytstream.headers){
-            headers[header] = ytstream.headers[header];
+            for(const header in ytstream.headers){
+                headers[header] = ytstream.headers[header];
+            }
+
+            headers['cookie'] = ytstream.agent.jar.getCookieStringSync('https://www.youtube.com');
+
+            var request_url = `https://www.youtube.com/playlist?list=${listId}&has_verified=1`;
+
+            var response;
+            try{
+                response = await requestPlayList(request_url, headers, ytstream.agent);
+            } catch (err){
+                reject(err);
+            }
+
+            response = response.split('var ytInitialData = ')[1];
+            if(!response) return reject(`The YouTube playlist has no initial data response`);
+
+            response = response.split(';</script>')[0];
+            if(!response) return reject(`The YouTube playlist has no initial data response`);
+
+            var json = response;
+            try{
+                json = JSON.parse(json);
+            } catch {}
+            
+            var alerts = (json.alerts || []);
+            alerts.push({alertRenderer: {}});
+            var errors = alerts.filter(a => {
+                return (a.alertRenderer || a.alertWithButtonRenderer).type === 'ERROR';
+            });
+            if(errors.length > 0) return reject(errors[0].alertRenderer.text.runs[0].text);
+            resolve(new Playlist(json, headers, listId));
+        } else if(ytstream.preference === 'api'){
+            if(typeof ytstream.apiKey !== 'string') return reject(`No valid API key has been set`);
+
+            const clientInfo = genClientInfo(undefined, ytstream.client);
+
+            request('https://www.youtube.com/youtubei/v1/browse?apiKey='+ytstream.apiKey+'&prettyPrint=false', {
+                method: 'POST',
+                body: JSON.stringify({...clientInfo.body, browseId: !listId.startsWith("VL") ? `VL${listId}` : listId}),
+                headers: clientInfo.headers
+            }, ytstream.agent, 0, false).then(res => {
+                let data;
+                try{
+                data = JSON.parse(res);
+                } catch {
+                reject(`Invalid response from the YouTube API`);
+                return;
+                }
+                resolve(new Playlist(data, clientInfo.headers, listId));
+            }).catch(reject);
         }
-
-        headers['cookie'] = ytstream.agent.jar.getCookieStringSync('https://www.youtube.com');
-
-        var request_url = `https://www.youtube.com/playlist?list=${listId}&has_verified=1`;
-
-        var response;
-        try{
-            response = await requestPlayList(request_url, headers, ytstream.agent);
-        } catch (err){
-            reject(err);
-        }
-
-        response = response.split('var ytInitialData = ')[1];
-        if(!response) return reject(`The YouTube playlist has no initial data response`);
-
-        response = response.split(';</script>')[0];
-        if(!response) return reject(`The YouTube playlist has no initial data response`);
-
-        var json = response;
-        try{
-            json = JSON.parse(json);
-        } catch {}
-        
-        var alerts = (json.alerts || []);
-        alerts.push({alertRenderer: {}});
-        var errors = alerts.filter(a => {
-            return (a.alertRenderer || a.alertWithButtonRenderer).type === 'ERROR';
-        });
-        if(errors.length > 0) return reject(errors[0].alertRenderer.text.runs[0].text);
-        resolve(new Playlist(json, headers));
     });
 }
 
@@ -79,20 +101,20 @@ function requestPlayList(url, headers, agent){
             return reject(err);
         }
         
-        while(_url((res.headers.location || '')) !== false){
-            res = await requestCallback(res.headers.location, options, agent, false);
+        while(_url((res.stream.headers.location ?? '')) !== false){
+            res = await requestCallback(res.stream.headers.location, options, agent, false);
         }
 
         var response = '';
-        res.on('data', d => {
+        res.stream.on('data', d => {
             response += d;
         });
 
-        res.on('end', () => {
+        res.stream.on('end', () => {
             resolve(response);
         });
 
-        res.on('error', err => {
+        res.stream.on('error', err => {
             reject(err);
         });
     });
