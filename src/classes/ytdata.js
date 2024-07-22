19206596f1f8c17c813ebea04b99e4ca252e4f36
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-07-22 21:22:20 +0200

    Update ytdata.js

--- src/classes/ytdata.js
+++ src/classes/ytdata.js
@@ -1,25 +1,34 @@
 class YouTubeData{
-    constructor(data, html5player, headers){
-        var videoDetails = data.videoDetails;
-        var microformat = data.microformat.playerMicroformatRenderer;
+    constructor(data, html5player, headers, clientInfo){
+        let videoDetails = data.videoDetails;
         this.id = videoDetails.videoId;
         this.url = `https://www.youtube.com/watch?v=${videoDetails.videoId}`;
         this.author = videoDetails.author;
         this.title = videoDetails.title;
         this.description = videoDetails.shortDescription;
-        this.embed_url = typeof microformat.embed === 'object' ? microformat.embed.iframeUrl : `https://www.youtube.com/embed/${videoDetails.videoId}`;
-        this.family_safe = microformat.isFamilySafe;
-        this.available_countries = microformat.availableCountries;
-        this.category = microformat.category;
+        this.embed_url = `https://www.youtube.com/embed/${videoDetails.videoId}`;
         this.thumbnails = videoDetails.thumbnail.thumbnails;
         this.default_thumbnail = videoDetails.thumbnail.thumbnails[videoDetails.thumbnail.thumbnails.length - 1];
-        const uploadedTime = new Date(microformat.publishDate);
-        this.uploaded = `${uploadedTime.getDate()}-${uploadedTime.getMonth() + 1}-${uploadedTime.getFullYear()}`;
-        this.uploadedTimestamp = uploadedTime.getTime();
         this.duration = parseInt(videoDetails.lengthSeconds) * 1000;
         this.views = parseInt(videoDetails.viewCount);
 
-        var viewsText = videoDetails.viewCount.toString();
+        if(typeof data.microformat === 'object'){
+            let microformat = data.microformat.playerMicroformatRenderer;
+            const uploadedTime = new Date(microformat.publishDate);
+            this.uploaded = `${uploadedTime.getDate()}-${uploadedTime.getMonth() + 1}-${uploadedTime.getFullYear()}`;
+            this.uploadedTimestamp = uploadedTime.getTime();
+            this.family_safe = microformat.isFamilySafe;
+            this.available_countries = microformat.availableCountries;
+            this.category = microformat.category;
+        } else {
+            this.uploaded = null;
+            this.uploadedTimestamp = 0;
+            this.family_safe = null;
+            this.available_countries = [];
+            this.category = null;
+        }
+
+        let viewsText = videoDetails.viewCount.toString();
         viewsText = viewsText.split('').reverse();
         viewsText = viewsText.reduce((arr, number, index, defaultArray) => {
             if((index + 1) % 3 === 0 && index !== 0 && (index + 1) !== defaultArray.length){
@@ -40,6 +49,7 @@ class YouTubeData{
         };
         this.formats = [];
         this.html5player = html5player;
+        this.clientInfo = clientInfo;
 
         this.formats.push(...(data.streamingData.formats || []));
         this.formats.push(...(data.streamingData.adaptiveFormats || []));
