49f8fc9a2e424bfe1cb65905e1b06c64b7ebb231
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-07-22 21:22:37 +0200

    Update searchdata.js

--- src/classes/searchdata.js
+++ src/classes/searchdata.js
@@ -1,11 +1,13 @@
 class Video{
-    constructor(data, headers){
+    constructor(data, headers, api){
         this.id = data.videoId;
         this.url = `https://www.youtube.com/watch?v=${this.id}`;
-        this.title = data.title.runs[0].text;
-        this.author = data.ownerText.runs[0].text;
-        this.channel_id = data.ownerText.runs[0].navigationEndpoint.browseEndpoint.browseId;
-        this.channel_url = `https://www.youtube.com${data.ownerText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url}`;
+        this.title = Array.isArray(data.title?.runs) ? data.title.runs[0].text : '';
+        let authorObj = (data.ownerText ?? data.longBylineText);
+        this.author = Array.isArray(authorObj?.runs) ? authorObj.runs[0].text : '';
+        let channelObj = (data.ownerText ?? data.longBylineText);
+        this.channel_id = Array.isArray(channelObj?.runs) ? channelObj.runs[0].navigationEndpoint.browseEndpoint.browseId : null;
+        this.channel_url = this.channel_id ? `https://www.youtube.com/channel/`+this.channel_id : null;
         this.user_agent = headers['user-agent'];
         if(typeof headers['cookie'] === 'string'){
             this.cookie = headers['cookie'];
@@ -14,13 +16,17 @@ class Video{
         }
 
         if(data.lengthText){
-            this.length_text = data.lengthText.simpleText;
-            const length = this.lengthText.split(':').reverse();
+            if(typeof data.lengthText.simpleText === 'string'){
+                this.length_text = data.lengthText.simpleText;
+            } else if(Array.isArray(data.lengthText?.runs)){
+                this.length_text = data.lengthText.runs[0].text;
+            }
+            const length = this.length_text.split(':').reverse();
             var timestampLength = 0;
             for(var i = 0; i < length.length; i++){
-            const l = length[i];
-            if(i < 3) timestampLength += l * Math.pow(60, i) * 1000;
-            else if(i === 3) timestampLength += l * 24 * Math.pow(60, i) * 1000;
+                const l = length[i];
+                if(i < 3) timestampLength += l * Math.pow(60, i) * 1000;
+                else if(i === 3) timestampLength += l * 24 * Math.pow(60, i) * 1000;
             }
             this.length = timestampLength;
         } else {
@@ -29,8 +35,12 @@ class Video{
         }
 
         if(data.viewCountText){
-            this.views_text = data.viewCountText.simpleText;
-            if(data.viewCountText.simpleText) this.views = parseInt(data.viewCountText.simpleText.toLowerCase().split(',').join('').split(' ')[0]);
+            if(typeof data.viewCountText.simpleText === 'string'){
+                this.views_text = data.viewCountText.simpleText;
+            } else if(Array.isArray(data.viewCountText?.runs)){
+                this.views_text = data.viewCountText.runs[0].text;
+            }
+            if(this.views_text) this.views = parseInt(this.views_text.toLowerCase().split(',').join('').split(' ')[0]);
             else this.views_text = 'Unknown views';
         } else {
             this.view_text = 'Unknown views';
